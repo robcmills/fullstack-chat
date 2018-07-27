bfd5e8277e53ef6c934e4693ec27728f5b194553
robcmills <rob@buildingconnected.com>
2018-07-26 21:54:11 -0700

    Initial commit to client branch

--- README.md
+++ README.md
@@ -7,3 +7,10 @@ A simple fullstack chat app
 - Install Docker globally
 - From the root of this repo run `docker-compose up fullstack`
 - Visit http://localhost:13000/test
+
+### Client Development
+
+- Change directory into `frontend-react`
+- Ensure you are using the correct version of node (nvm)
+- Install dependencies `npm i`
+- Start the client dev server `npm start`
--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -4,19 +4,9 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h1>Hello</h1>
-        <pre>state = {JSON.stringify(this.state, undefined, '  ')}</pre>
+        <h1>Hello 2</h1>
       </div>
     );
-  }
-  componentDidMount() {
-    fetch('/test').then((res) => {
-      return res.json();
-    }).then((res) => {
-      this.setState({res});
-    }).catch((err) => {
-      this.setState({err});
-    });
   }
 }
 
